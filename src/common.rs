27a2dd58911b73d9fc56558f1f21c4eea7b2b5fc
鬼鬼Sama <54203072+Akuma-real@users.noreply.github.com>
2025-05-08 12:55:30 +0800

    移除连接页面中的不必要文本和交互元素，并更新 API 服务器 URL。

--- src/common.rs
+++ src/common.rs
@@ -958,7 +958,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://221.236.27.74:22024".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
